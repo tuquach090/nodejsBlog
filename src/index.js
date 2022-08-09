6f85f63ee60477c9d842bc7c5aa481680db6de4b
Tuquach090 <tuquach090@gmail.com>
2022-08-09 11:09:04 +0700

    update

--- src/index.js
+++ src/index.js
@@ -5,19 +5,31 @@ const { engine } = require('express-handlebars');
 const app = express();
 const port = 3000;
 const route = require("./resource/routes");
+const db = require("./resource/config/db");
+
+// Connect Db
+db.connect();
 
 // Config use file
 app.use(express.static(path.join(__dirname, 'public')));
+
+
 // HTTP loger
 // app.use(morgan("combined"));
+
+
 // Template Engine
 app.engine("hbs", engine({
     extname: ".hbs"
 }));
+
+
 // Midlewere get request post
 app.use(express.urlencoded({
     extended: true
 }));
+
+
 // Set View Engine
 app.set("view engine", "hbs");
 app.set("views", path.join(__dirname, "/resource/views"));
--- src/resource/routes/news.js
+++ src/resource/routes/news.js
@@ -5,6 +5,6 @@ const newsController = require("../app/controllers/NewsController");
 
 
 router.use("/:slug", newsController.show);
-router.use("/", newsController.index);
+router.use("/", newsController.index);  
 
 module.exports = router;
